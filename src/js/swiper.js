a1b2883a9e2db71d26fa0d35f317940176d61565
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-11-02 22:51:15 +0200

    fix swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -13,27 +13,27 @@ export const updateSwiperMarkup = {
 export const mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'horizontal',
-  effect: 'coverflow',
-  grabCursor: true,
+  effect: 'slide',
+  // grabCursor: true,
   centeredSlides: true,
   slidesPerView: 3,
-  loop: true,
-  speed: 300,
+  // loop: true,
+  // speed: 300,
   spaceBetween: 30,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
+  // coverflowEffect: {
+  //   rotate: 50,
+  //   stretch: 0,
+  //   depth: 100,
+  //   modifier: 1,
+  //   slideShadows: true,
+  // },
+  // autoplay: {
+  //   delay: 3000,
+  // },
 
   // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
 });
