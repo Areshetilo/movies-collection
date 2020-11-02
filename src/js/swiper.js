0872e3a10cacedad010fa0d18dc8970602db7690
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-11-02 23:02:20 +0200

    fixed manual settings for swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -14,22 +14,22 @@ export const mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   direction: 'horizontal',
   effect: 'slide',
-  // grabCursor: true,
+  grabCursor: true,
   centeredSlides: true,
   slidesPerView: 3,
-  // loop: true,
-  // speed: 300,
+  loop: true,
+  speed: 300,
   spaceBetween: 30,
-  // coverflowEffect: {
-  //   rotate: 50,
-  //   stretch: 0,
-  //   depth: 100,
-  //   modifier: 1,
-  //   slideShadows: true,
-  // },
-  // autoplay: {
-  //   delay: 3000,
-  // },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
 
   // If we need pagination
   // pagination: {
