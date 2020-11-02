174b70f8cc45bb05df07148e85a299323fb6277a
Aleksandr Reshetilo <aleksandr.reshetilo@gmail.com>
2020-11-02 17:07:26 +0200

    added html gor swiper

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import {
   checkFilmHandler,
   closeModalEscapeHandler,
 } from './js/components/modal/modalListener';
-import mySwiper from './js/components/swiper';
+import Swiper from 'swiper/bundle';
 
 function loadData() {
   return new Promise((resolve) => {
@@ -41,6 +41,40 @@ loadData().then(() => {
 loadOnScroll();
 console.log('running populars fetch');
 fetchedMoviesHandler('popular');
+
+let mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  loop: true,
+  speed: 300,
+  spaceBetween: 30,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 const submitHandler = (e) => {
   e.preventDefault();
